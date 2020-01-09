55715474a178b3fc22dd8a18fe5386ff235b594a
ac2pic <ac2pic@gmail.com>
2020-01-09 14:50:34 -0600

    Fixed naming issue

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-GOOS="linux" GOARCH="amd64" go build -o "./bin/ccmu" 
-GOOS="windows" GOARCH="amd64" go build -o "./bin/ccmu.exe"
-GOOS="darwin" GOARCH="amd64" go build -o "./bin/ccmu_mac"
+GOOS="linux" GOARCH="amd64" go build -o "./bin/ccms" 
+GOOS="windows" GOARCH="amd64" go build -o "./bin/ccms.exe"
+GOOS="darwin" GOARCH="amd64" go build -o "./bin/ccms_mac"
 
 cd bin
 
@@ -8,12 +8,12 @@ cd bin
 
 export PATH=$PATH:"/C/Program Files/7-zip/"
 
-sha256sum ccmu_mac > checksum_sha256_mac.txt
-7z.exe a -ttar -so -an ccmu_mac checksum_sha256_mac.txt | 7z.exe a -si ccmu_mac.tgz
+sha256sum ccms_mac > checksum_sha256_mac.txt
+7z.exe a -ttar -so -an ccms_mac checksum_sha256_mac.txt | 7z.exe a -si ccms_mac.tgz
 
 
-sha256sum ccmu  > checksum_sha256_linux.txt
-7z.exe a -ttar -so -an ccmu checksum_sha256_linux.txt | 7z.exe a -si ccmu_linux.tgz
+sha256sum ccms  > checksum_sha256_linux.txt
+7z.exe a -ttar -so -an ccms checksum_sha256_linux.txt | 7z.exe a -si ccms_linux.tgz
 
 
 
