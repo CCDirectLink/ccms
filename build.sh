ce488fcd3b9be12fe218d9cc39edef6579e96c01
ac2pic <ac2pic@gmail.com>
2020-01-09 14:29:53 -0600

    Updated the build script to compress the mac and linux files

--- build.sh
+++ build.sh
@@ -5,6 +5,17 @@ GOOS="darwin" GOARCH="amd64" go build -o "./bin/ccmu_mac"
 cd bin
 
 
-sha256sum ccmu.exe ccmu ccmu_mac > checksum_sha256.txt
+
+export PATH=$PATH:"/C/Program Files/7-zip/"
+
+sha256sum ccmu_mac > checksum_sha256_mac.txt
+7z.exe a -ttar -so -an ccmu_mac checksum_sha256_mac.txt | 7z.exe a -si ccmu_mac.tgz
+
+
+sha256sum ccmu  > checksum_sha256_linux.txt
+7z.exe a -ttar -so -an ccmu checksum_sha256_linux.txt | 7z.exe a -si ccmu_linux.tgz
+
+
+
 
 cd ..
