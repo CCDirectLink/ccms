1a148c1a91156f4d27a831562d953ce66ec62e39
ac2pic <ac2pic@gmail.com>
2020-02-05 21:55:49 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -11,15 +11,15 @@ export PATH=$PATH:"/C/Program Files/7-zip/"
 mkdir checksums
 
 sha256sum ccms_mac > ./checksums/checksum_sha256_mac.txt
-7z.exe a -ttar -so -an ccms_mac ./checksums/checksum_sha256_mac.txt | 7z.exe a -si ./compressed/ccms_mac.tgz
+7z.exe a -ttar -so -an ccms_mac ./checksums/checksum_sha256_mac.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_mac.tgz
 
 
-sha256sum ccms  > ./checksums/checksum_sha256_linux.txt
-7z.exe a -ttar -so -an ccms ./checksums/checksum_sha256_linux.txt | 7z.exe a -si ./compressed/ccms_linux.tgz
+sha256sum ccms > ./checksums/checksum_sha256_linux.txt
+7z.exe a -ttar -so -an ccms ./checksums/checksum_sha256_linux.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_linux.tgz
 
 sha256sum ccms.exe > ./checksums/checksum_sha256_windows.txt
 
-7z.exe a ./compressed/ccms_windows.zip ccms.exe ./checksums/checksum_sha256_windows.txt
+7z.exe a ./compressed/ccms_windows.zip ccms.exe ./checksums/checksum_sha256_windows.txt ../LICENSE
 
 
 
