ffcbc36f64157aec2cc27dd57ec411a189d40ab2
ac2pic <ac2pic@gmail.com>
2020-02-23 18:48:00 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -1,26 +1,22 @@
+#!/bin/bash
+
 GOOS="linux" GOARCH="amd64" go build -o "./bin/ccms" 
 GOOS="windows" GOARCH="amd64" go build -o "./bin/ccms.exe"
 GOOS="darwin" GOARCH="amd64" go build -o "./bin/ccms_mac"
 
 cd bin
 
-
-
-export PATH=$PATH:"/C/Program Files/7-zip/"
-
-mkdir checksums
+mkdir -p checksums
 
 sha256sum ccms_mac > ./checksums/checksum_sha256_mac.txt
-7z.exe a -ttar -so -an ccms_mac ./checksums/checksum_sha256_mac.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_mac.tgz
+7z a -ttar -so -an ccms_mac ./checksums/checksum_sha256_mac.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_mac.tgz
 
 
 sha256sum ccms > ./checksums/checksum_sha256_linux.txt
-7z.exe a -ttar -so -an ccms ./checksums/checksum_sha256_linux.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_linux.tgz
+7z a -ttar -so -an ccms ./checksums/checksum_sha256_linux.txt ../LICENSE | 7z.exe a -si ./compressed/ccms_linux.tgz
 
 sha256sum ccms.exe > ./checksums/checksum_sha256_windows.txt
 
-7z.exe a ./compressed/ccms_windows.zip ccms.exe ./checksums/checksum_sha256_windows.txt ../LICENSE
-
-
+7z a ./compressed/ccms_windows.zip ccms.exe ./checksums/checksum_sha256_windows.txt ../LICENSE
 
 cd ..
